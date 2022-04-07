d15ebec4082420a8d15655094266982907c5c55d
Julie Combeau <combeau.julie@gmail.com>
2022-04-07 11:37:42 +0200

    figth completed : heracles win !

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 const Fighter = require("./src/Fighter.js");
+const Weapon = require("./src/Weapon.js");
+const Shield = require("./src/Shield.js");
+
+const weapon = new Weapon("sword");
+const shield = new Shield("toto");
 
 /** Create Heracles  */
-const heracles = new Fighter("🧔 Heracles", 20, 6);
+const heracles = new Fighter(
+  "Heracles",
+  20,
+  6,
+  weapon.damage,
+  shield.protection
+);
 
 /** Create the opponent  */
-const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
+const boar = new Fighter("Erymanthian Boar", 25, 12);
 
 /**
  * Helper to produce the result of a round
  */
 const roundDisplay = (fighter1, fighter2) => {
-  return `${fighter1.name} 🗡️  ${fighter2.name} 💙 ${fighter2.name} : ${fighter2.life}`;
+  return `${fighter1.name} attack ${fighter2.name}  ${fighter2.name} : ${fighter2.life}`;
 };
 
 /**
  * Helper to dispatch fighters between winner and loser
  */
 const score = (fighter1, fighter2) => {
-  return fighter1.isAlive() ? {
-    winner: fighter1,
-    loser: fighter2,
-  } : {
-    winner: fighter2,
-    loser: fighter1
-  };
+  return fighter1.isAlive()
+    ? {
+        winner: fighter1,
+        loser: fighter2,
+      }
+    : {
+        winner: fighter2,
+        loser: fighter1,
+      };
 };
+
+// console.log(roundDisplay(heracles, boar));
+// console.log(heracles);
+// console.log(boar);
+
+{
+  let round = 0;
+
+  while (heracles.isAlive() && boar.isAlive()) {
+    console.log(` Round #${round}`);
+
+    heracles.fight(boar);
+    console.log(roundDisplay(heracles, boar));
+
+    boar.fight(heracles);
+    console.log(roundDisplay(boar, heracles));
+
+    round++;
+  }
+
+  const result = score(heracles, boar);
+
+  console.log(` ${result.loser.name} is dead`);
+  console.log(` ${result.winner.name} wins ( ${result.winner.life} )`);
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,34 +1,41 @@
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
-        this.name = name;
-        this.strength = strength;
-        this.dexterity = dexterity;
-        this.life = MAX_LIFE;
-    }
+  constructor(name, strength, dexterity, weapon = null, shield = null) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+    this.weapon = weapon;
+    this.shield = shield;
+  }
 
+  // Launch a fight
+  fight(defender) {
+    const attackPoints = this.getRandomInt(this.getDamage());
 
-    // Launch a fight
-    fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+    const damages = Math.max(attackPoints - defender.getDefense(), 0);
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+    defender.life = Math.max(defender.life - damages, 0);
+  }
 
-        defender.life = Math.max(defender.life - damages, 0);
-    }
+  getDamage() {
+    return this.strength + this.weapon;
+  }
 
+  getDefense() {
+    return this.dexterity + this.shield;
+  }
 
-    // Generate a random value between 1 and max
-    getRandomInt(max) {
-        return 1 + Math.floor(Math.random() * max);
-    }
+  // Generate a random value between 1 and max
+  getRandomInt(max) {
+    return 1 + Math.floor(Math.random() * max);
+  }
 
-
-    // Determine if a fighter is still alive
-    isAlive() {
-        return this.life > 0;
-    }
+  // Determine if a fighter is still alive
+  isAlive() {
+    return this.life > 0;
+  }
 }
 
 module.exports = Fighter;
